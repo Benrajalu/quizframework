6354efd18798f38cd0cc0a74fd78bb6358bdd253
Benoit R <rajalubenoit@gmail.com>
2020-01-25 13:56:04 +0100

    questions for phase 1

--- src/data/phase1.js
+++ src/data/phase1.js
@@ -1,9 +1,54 @@
-import waldo from './media/waldo.jpg';
-import emile from './media/emile.jpg';
-import usualSuspects from './media/usualSuspects.jpeg';
 import kwai from './media/kwai.jpg';
 
 export const SLIDES = [
+  {
+    type: 'question',
+    question:
+      'Quel acteur tenait le rôle de Tony Stark dans le Marvel Cinematic Universe ?',
+    answer: 'Robert Downey Jr.'
+  },
+  {
+    type: 'question',
+    question: 'Quelles sont les 4 maisons de Poudlard ?',
+    answer:
+      'Gryffondor / Griffindor, Serpentard / Slythernin, Pouffesoufle / Hufflepuff, Serdaigle / Ravenclaw'
+  },
+  {
+    type: 'question',
+    question: 'Au tennis, en combien de sets gagants se jouent les tournois du Grand Chelem ?',
+    answer:
+      '2 pour les femmes, 3 pour les hommes'
+  },
+  {
+    type: 'question',
+    question: "Combien d'équipes sont en compétition dans le Ligue 1 de foot ?",
+    answer:
+      '20'
+  },
+  {
+    type: 'question',
+    question: "Pour quelle série de livres l'auteur Philip Pullman est-il connu ?",
+    answer:
+      'La croisée des mondes'
+  },
+  {
+    type: 'question',
+    question: 'Quelles sont les 4 maisons de Poudlard ?',
+    answer:
+      'Gryffondor / Griffindor, Serpentard / Slythernin, Pouffesoufle / Hufflepuff, Serdaigle / Ravenclaw'
+  },
+  {
+    type: 'question',
+    question: 'Quelles sont les 4 maisons de Poudlard ?',
+    answer:
+      'Gryffondor / Griffindor, Serpentard / Slythernin, Pouffesoufle / Hufflepuff, Serdaigle / Ravenclaw'
+  },
+  {
+    type: 'image',
+    question: 'Quel est ce pont ?',
+    image: kwai,
+    answer: 'Le pont de la rivière Kwai'
+  },
   {
     type: 'question',
     question: 'Qui chante',
@@ -15,80 +60,5 @@ export const SLIDES = [
     question: 'Qui chante',
     videoTube: 'https://www.youtube.com/watch?v=O4irXQhgMqg',
     answer: 'Jeff Bezos avec $145.3 Milliards.'
-  },
-  {
-    type: 'question',
-    question: "Qui est l'homme le plus riche du monde ?",
-    answer: 'Jeff Bezos avec $145.3 Milliards.'
-  },
-  {
-    type: 'question',
-    question: 'Qui est la femme la plus riche du monde ?',
-    answer: 'Françoise Bettencourt Meyers avec $49.3 Milliards.'
-  },
-  {
-    type: 'image',
-    question: 'Quel est ce pont ?',
-    image: kwai,
-    answer: 'Le pont de la rivière Kwai'
-  },
-  {
-    type: 'question',
-    question: 'Quelle ville est la capitale de la France...en 1940 ?',
-    answer: 'Vichy.'
-  },
-  {
-    type: 'question',
-    question: 'Quel artiste célèbre a créé le logo de Chupa Chups ?',
-    answer: 'Salvador Dali'
-  },
-  {
-    type: 'image',
-    question: 'Qui est cet homme ?',
-    image: emile,
-    answer: 'Emile, Le tueur'
-  },
-  {
-    type: 'question',
-    question: 'Qui a écrit "American Gods" ?',
-    answer: 'Neil Gaiman'
-  },
-  {
-    type: 'question',
-    question: 'Qui a gagné le prix Nobel de la paix en 2018 ?',
-    answer: 'Denis Mukwege Mukengere, Nadia Murad'
-  },
-  {
-    type: 'image',
-    question: 'Où est Charlie ?',
-    image: waldo,
-    answer: 'En haut à gauche'
-  },
-  {
-    type: 'question',
-    question: 'Quelle est la première entreprise du CAC 40 (en CA) ?',
-    answer: 'LVMH'
-  },
-  {
-    type: 'question',
-    question:
-      'Combien coûte un sandwich triangle genre jambon-fromage au wagon bar SNCF ?',
-    answer: '6,40€ https://lebar.sncf.com/'
-  },
-  {
-    type: 'question',
-    question: 'En quelle année a été signé le traité de Rome ?',
-    answer: '1958'
-  },
-  {
-    type: 'question',
-    question: 'Quelle équipe de foot a gagné le plus de coupes du monde ?',
-    answer: 'Le Brésil'
-  },
-  {
-    type: 'image',
-    question: 'Quel est le titre de ce film ?',
-    image: usualSuspects,
-    answer: 'Usual Suspects'
   }
 ];
